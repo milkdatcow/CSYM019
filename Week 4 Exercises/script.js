b0b228a3d5a286dd4b6688cad011c894951fe4d4
Dyl <bowyer008@gmail.com>
2025-02-19 09:59:32 +0000

    week 4 exercises

---
+++ Week 4 Exercises/script.js
@@ -0,0 +1,29 @@
+/*
+function alertfive(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+}
+//alertfive();
+//alertfive();
+*/
+
+function updateheader(){
+    var header = document.getElementById('header');
+    header.firstChild.nodeValue = 'New Header';
+}
+function updatetext(){
+    var text = document.getElementById('text');
+    text.firstChild.nodeValue = 'New Paragraph';
+}
+function addlisteners(){
+    var header = document.getElementById('header');
+    var text = document.getElementById('text');
+    header.addEventListener('mouseleave',updateheader)
+    text.addEventListener('mouseenter',updatetext)
+
+}
+
+document.addEventListener('DOMContentLoaded', addlisteners)
